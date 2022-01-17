51f09b4e697a43e6d70066dc9c33d36874ef174e
moali-98 <moali_98@hotmail.com>
2022-01-17 11:34:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener("submit" , (event)=>{
+    console.log("Validating")
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+
+    let request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:3000/authorize");
+    //request.send(new FormData(formElement));
+    console.log("Validated")
+
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(`user=${user}&password=${password}`);
+
+    event.preventDefault()
+})
